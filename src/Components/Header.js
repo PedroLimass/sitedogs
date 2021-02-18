c49e5ed5ebf1c882ab27402415ff3edda4a17b25
Pedro Lima <48936052+PedroLimass@users.noreply.github.com>
2021-02-17 23:15:03 -0300

    Add header e  assets

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import style from './Header.module.css';
+import { ReactComponent as Dogs } from '../Assets/dogs.svg';
 
 const Header = () => {
   return (
-    <div className={style.header}>
-      <nav className='container'>
-        <Link to='/'>Home</Link>
-        <Link to='/login'>Login / Criar</Link>
+    <header className={style.header}>
+      <nav className={`${style.nav} container`}>
+        <Link className={style.logo} to='/' aria-label='Dogs - Home'>
+          <Dogs />
+        </Link>
+        <Link className={style.login} to='/login'>
+          Login / Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 };
 
