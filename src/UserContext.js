f72f91c9c8aedba4b336443b875ca916ee7a6b55
Pedro Lima <48936052+PedroLimass@users.noreply.github.com>
2021-02-27 15:14:49 -0300

    Apartir do useEffect criando uma função para verificar o login junto a api

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,14 +1,34 @@
 import { getSuggestedQuery } from '@testing-library/react';
 import React, { Children } from 'react';
-import { TOKEN_POST, USER_GET } from './api';
+import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from './api';
 
 export const UserContext = React.createContext();
 
 export const UserStorage = ({ children }) => {
   const [data, setData] = React.useState(null);
   const [login, setLogin] = React.useState(null);
-  const [loading, setLoading] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    async function autoLogin() {
+      const token = window.localStorage.getItem('token');
+      if (token) {
+        try {
+          setError(null);
+          setLoading(true);
+          const { url, options } = TOKEN_VALIDATE_POST(token);
+          const response = await fetch(url, options);
+          if (!response.ok) throw new Error('Token invalido');
+          await getUser(token);
+        } catch (err) {
+        } finally {
+          setLoading(false);
+        }
+      }
+    }
+    autoLogin();
+  }, []);
 
   async function getUser(token) {
     const { url, options } = USER_GET(token);
