24925a2109a1e061473bd192c3dc32f99891efed
Pedro Lima <48936052+PedroLimass@users.noreply.github.com>
2021-03-03 11:16:35 -0300

    add estilo ao forms

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -5,6 +5,8 @@ import Input from '../Forms/Input';
 import useForm from '../../Hooks/useForm';
 import { UserContext } from '../../UserContext';
 import Error from '../Helper/Error';
+import styles from './LoginForm.module.css';
+import stylesBtn from '../Forms/Button.module.css';
 
 const LoginForm = () => {
   const username = useForm();
@@ -22,14 +24,23 @@ const LoginForm = () => {
     <div>
       <section className='animeLeft'>
         <h1 className='title'>Login</h1>
-        <form action='' onSubmit={handleSubmit}>
+        <form className={styles.form} onSubmit={handleSubmit}>
           <Input label='Usuário' type='text' name='username' {...username} />
           <Input label='Senha' type='password' name='password' {...password} />
           {loading ? <Button disabled>Entrar</Button> : <Button>Entrar</Button>}
           <Error error={error} />
           {error && <p>{error}</p>}
         </form>
-        <Link to='/login/criar'>Cadastro</Link>
+        <Link className={styles.perdeu} to='/login/perdeu'>
+          Esqueceu a Senha?
+        </Link>
+        <div className={styles.cadastro}>
+          <h2 className={styles.subtitle}>Cadastre-se</h2>
+          <p>Ainda não possui conta? Cadastre-se no site.</p>
+          <Link className={stylesBtn.button} to='/login/criar'>
+            Cadastro
+          </Link>
+        </div>
       </section>
     </div>
   );
